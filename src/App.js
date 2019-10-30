9473b8bd197abe615c67b71a6cf72c443e9e5681
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-30 01:27:43 -0300

    criando barra  de navegacao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import NavBar from './components/layout/NavBar';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+
+  useEffect(() => {
+
+  }, []);
   return (
     <div className="App">
-      <h1>Marlon</h1>
+      <NavBar />
     </div>
   )
 }
