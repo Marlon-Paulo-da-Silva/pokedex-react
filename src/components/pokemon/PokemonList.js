770681bc6b7595ad035383db6bf59deb6021cfd3
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-10-30 01:48:34 -0300

    criando dashboard com list e cards"

---
+++ src/components/pokemon/PokemonList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import PokemonCard from './PokemonCard';
+
+export default class PokemonList extends Component {
+    render() {
+        return (
+            <div>
+                <PokemonCard />
+            </div>
+        )
+    }
+}
