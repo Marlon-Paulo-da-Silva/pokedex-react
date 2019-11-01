393ba4419f871e2f601594854b262ff01e4433db
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-01 00:55:55 -0300

    adicionando conexao com axios

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import PokemonCard from './PokemonCard';
 
+
 export default class PokemonList extends Component {
+
+    state = {
+        url: "https://pokeapi.co/api/v2/pokemon/",
+        pokemon: null
+    };
+
+    async componentDidMount(){
+        const res = await axios.get(this.state.url);
+        this.setState({pokemon: res.data['results']});
+    }
+
+
     render() {
         return (
-            <div>
+            <div className="row">
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
+                <PokemonCard />
                 <PokemonCard />
             </div>
         )
