2f800a639746d444312197bd424a24f79d1e9bf7
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-01 15:25:01 -0300

    pegando as  descriçoes

--- src/components/pokemon/Pokemon.js
+++ src/components/pokemon/Pokemon.js
@@ -19,7 +19,7 @@ export default class Pokemon extends Component {
         },
         height: "",
         weight: "",
-        eggGroup: "",
+        eggGroups: "",
         abilities: "",
         genderRatioMale: "",
         genderRatioFemale: "",
@@ -32,7 +32,7 @@ export default class Pokemon extends Component {
 
         //URLs para obter as informações dos pokemons
         const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
-        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon/pokemon-species/${pokemonIndex}/`;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
 
         //Pegando Informações do Pokemon
         const pokemonRes = await axios.get(pokemonUrl);
@@ -96,8 +96,44 @@ export default class Pokemon extends Component {
             .join(" ");
         }).join(', ');
 
-        //Obter Descrição do pokemon, Probabilidade de captura,EGGGroups, Proporção de genero, etapas para chocar
-        await axios.get(pokemonSpeciesUrl);
+        //Obter Descrição do pokemon, Probabilidade de captura,EGGroups, Proporção de genero, passos para chocar
+        await axios.get(pokemonSpeciesUrl).then(res => {
+            let description = '';
+            res.data.flavor_text_entries.some(flavor => {
+                if(flavor.language.name === 'en'){
+                    description = flavor.flavor_text;
+                    return;
+                }
+            });
+
+            const femaleRate = res.data['gender_rate'];
+            const genderRatioFemale = 12.5 * femaleRate;
+            const genderRatioMale = 12.5 * (8 - femaleRate);
+
+            const catchRate = Math.round((100/255) * res.data['capture_rate']);
+
+            const eggGroups = res.data["egg_groups"].map(  group => {
+                return group.name
+                .toLowerCase()
+                .split("-")
+                .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                .join(" ");
+            }).join(', ');
+
+            const hatchSteps = 255 * (res.data['hatch_counter'] + 1);
+
+
+            this.setState(
+                description,
+                genderRatioFemale,
+                genderRatioMale,
+                catchRate,
+                eggGroups,
+                hatchSteps
+            );
+
+
+        });
     }
 
     render() {
