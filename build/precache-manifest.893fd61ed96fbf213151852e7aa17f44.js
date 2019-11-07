b6dddd1191a55c368b10e4fd964d2b0264bb1db9
Marlon Paulo <marlon.paulo.silva@outlook.com>
2019-11-07 16:38:41 -0300

    ultimo commit

---
+++ build/precache-manifest.893fd61ed96fbf213151852e7aa17f44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7d2588f6951311a94bc81d8a0dc0f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8338246ec0311deca78b",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "7b840845b54926cfce83",
+    "url": "/static/css/main.e829c3c1.chunk.css"
+  },
+  {
+    "revision": "8338246ec0311deca78b",
+    "url": "/static/js/2.edb170d1.chunk.js"
+  },
+  {
+    "revision": "7b840845b54926cfce83",
+    "url": "/static/js/main.5fb790ac.chunk.js"
+  },
+  {
+    "revision": "23e11197eb0ec2d8596f",
+    "url": "/static/js/runtime-main.d17f8506.js"
+  },
+  {
+    "revision": "f9fc704b0d7a341262a6b6041407eb98",
+    "url": "/static/media/spinner.f9fc704b.gif"
+  }
+]);
